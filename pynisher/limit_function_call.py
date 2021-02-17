c6884007cbef58f0bf24219a4c26d773cee1651d
chico <francisco.rivera.valverde@gmail.com>
2021-02-17 20:10:14 +0100

    Added more debug msg

--- pynisher/limit_function_call.py
+++ pynisher/limit_function_call.py
@@ -57,7 +57,6 @@ class AnythingException(Exception):
 def killtree(pid, including_parent=True):
     parent = psutil.Process(pid)
     for child in parent.children(recursive=True):
-        print "child", child
         child.kill()
 
     if including_parent:
